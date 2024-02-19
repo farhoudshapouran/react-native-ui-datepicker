db2e15b1f5d2273fbb4cbdf8fa431cfe8e31422e
Sebastian Markgraf <sebastian.markgraf@code-hive.de>
2024-02-19 14:47:28 +0100

    fix(test): fix testcase should not depend on today

--- src/__tests__/api.test.tsx
+++ src/__tests__/api.test.tsx
@@ -10,11 +10,13 @@ import 'dayjs/locale/tr';
 
 describe('API TESTS', () => {
   test('should display the passed date', () => {
-    const selectedDate = new Date();
+    const selectedDate = new Date(2020, 12, 19);
     const month = selectedDate.toLocaleString('en-US', { month: 'long' });
 
     render(<DateTimePicker mode="single" date={selectedDate} />);
     expect(screen.getByText(month)).toBeVisible();
+    expect(screen.getByText('19')).toBeVisible();
+    expect(screen.getByText('2020')).toBeVisible();
   });
 
   // test('minDate should be applied after init', () => {
