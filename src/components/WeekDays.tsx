36e04b339131dfbea16561fff43d30ee0555ed9e
Stefan J <stefanjovanovic1801@hotmail.com>
2025-02-07 00:47:18 +0100

    fix: resolve 'React is not defined' error

--- src/components/WeekDays.tsx
+++ src/components/WeekDays.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import React, { memo } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { getWeekdaysMin } from '../utils';
 import { CalendarThemeProps } from '../types';
