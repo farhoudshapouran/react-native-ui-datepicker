2cabe2fa05c238598b37767eda64825e9b59bc69
kingsley-eneris <kingsley@eneris.ca>
2025-03-14 03:08:28 -0700

    fix: resolve 'React is not defined' error

--- src/components/time-picker/period-web.tsx
+++ src/components/time-picker/period-web.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import React, { memo } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { ClassNames, Styles } from '../../types';
 import { isEqual } from 'lodash';
